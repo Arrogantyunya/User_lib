cdd2cedfbc5b17795b5f5c2b888c6f36da1bae8c
liujiahui <liujiahiu@qq.com>
2021-09-08 14:54:53 +0800

    更新了uartrt_analysic库，以支持不定长度的串口DMA接收

--- uart_analysis.c
+++ uart_analysis.c
@@ -17,28 +17,37 @@
 ** Version:				V1.0
 ** Descriptions:
 **--------------------------------------------------------------------------------------------------------*/
-#include "uart_irq_analysis.h"
+#include "uart_analysis.h"
 
-#if UART1_ENABLE_FLAG
+#if UART1_ENABLE_IRQRX
+#if UART1_ENABLE_DMARX
+extern DMA_HandleTypeDef hdma_usart1_rx;
+#endif
 uint8_t UART1_Rec_Data[UART1_REC_LEN_MAX] = {0x00};	//接收缓冲区数组
 uint8_t UART1_Rec_Len = 0;	//接收长度
 uint8_t UART1_Rec_Flag = false;//接收完成标志位
 #endif
 
-#if UART2_ENABLE_FLAG
+#if UART2_ENABLE_IRQRX
+#if UART2_ENABLE_DMARX
+extern DMA_HandleTypeDef hdma_usart2_rx;
+#endif
 uint8_t UART2_Rec_Data[UART2_REC_LEN_MAX] = {0x00};	//接收缓冲区数组
 uint8_t UART2_Rec_Len = 0;	//接收长度
 uint8_t UART2_Rec_Flag = false;//接收完成标志位
 #endif
 
-#if UART3_ENABLE_FLAG
+#if UART3_ENABLE_IRQRX
+#if UART3_ENABLE_DMARX
+extern DMA_HandleTypeDef hdma_usart3_rx;
+#endif
 uint8_t UART3_Rec_Data[UART3_REC_LEN_MAX] = {0x00};	//接收缓冲区数组
 uint8_t UART3_Rec_Len = 0;	//接收长度
 uint8_t UART3_Rec_Flag = false;//接收完成标志位
 #endif
 
 
-#if UART1_ENABLE_FLAG
+#if UART1_ENABLE_IRQRX
 /**
  * @Brief:串口中断处理函数
  * @Param:
@@ -48,7 +57,7 @@ uint8_t UART3_Rec_Flag = false;//接收完成标志位
  */
 void MY_UART1_IRQHandler(void)
 {
-
+#if (UART1_ENABLE_DMARX == false)
 	if(__HAL_UART_GET_FLAG(&huart1, UART_FLAG_RXNE) != RESET)
 	{
 		__HAL_UART_CLEAR_FLAG(&huart1,UART_FLAG_RXNE);
@@ -66,6 +75,24 @@ void MY_UART1_IRQHandler(void)
 //		__HAL_UART_CLEAR_FLAG(&huart1,UART_FLAG_IDLE);
 		UART1_Rec_Flag = true;
 	}
+#else
+  uint32_t temp;
+  if(__HAL_UART_GET_FLAG(&huart1, UART_FLAG_IDLE) != RESET)
+	{
+		/*注意：这里只能用__HAL_UART_CLEAR_IDLEFLAG来清除空闲中断标志位*/
+		__HAL_UART_CLEAR_IDLEFLAG(&huart1);
+		
+		temp = huart1.Instance->SR;  //清除状态寄存器SR,读取SR寄存器可以实现清除SR寄存器的功能
+		temp = huart1.Instance->DR; //读取数据寄存器中的数据
+		
+		HAL_UART_DMAStop(&huart1); //
+		
+    // temp = hdma_usart1_rx.Instance->NDTR;// 获取DMA中未传输的数据个数，NDTR寄存器分析见下面
+		temp = __HAL_DMA_GET_COUNTER(&hdma_usart1_rx);// 获取DMA中未传输的数据个数
+		UART1_Rec_Len = UART1_REC_LEN_MAX - temp;
+		UART1_Rec_Flag = true;
+	}
+#endif
 }
 
 
@@ -85,12 +112,15 @@ void UART1_Data_Analysis(void)
 		printf("接收的数据为：{%s}\r\n",UART1_Rec_Data);
 		memset(UART1_Rec_Data,'\0',sizeof(UART1_Rec_Data));
 		UART1_Rec_Len = 0;
+    #if UART1_ENABLE_DMARX
+    HAL_UART_Receive_DMA(&huart1,UART1_Rec_Data,UART1_REC_LEN_MAX);
+    #endif
 	}
 }
 #endif
 
 
-#if UART2_ENABLE_FLAG
+#if UART2_ENABLE_IRQRX
 /**
  * @Brief:串口中断处理函数
  * @Param:
@@ -100,7 +130,7 @@ void UART1_Data_Analysis(void)
  */
 void MY_UART2_IRQHandler(void)
 {
-
+#if (UART2_ENABLE_DMARX == false)
 	if(__HAL_UART_GET_FLAG(&huart2, UART_FLAG_RXNE) != RESET)
 	{
 		__HAL_UART_CLEAR_FLAG(&huart2,UART_FLAG_RXNE);
@@ -118,6 +148,24 @@ void MY_UART2_IRQHandler(void)
 //		__HAL_UART_CLEAR_FLAG(&huart2,UART_FLAG_IDLE);
 		UART2_Rec_Flag = true;
 	}
+#else
+  uint32_t temp;
+  if(__HAL_UART_GET_FLAG(&huart2, UART_FLAG_IDLE) != RESET)
+	{
+		/*注意：这里只能用__HAL_UART_CLEAR_IDLEFLAG来清除空闲中断标志位*/
+		__HAL_UART_CLEAR_IDLEFLAG(&huart2);
+		
+		temp = huart2.Instance->SR;  //清除状态寄存器SR,读取SR寄存器可以实现清除SR寄存器的功能
+		temp = huart2.Instance->DR; //读取数据寄存器中的数据
+		
+		HAL_UART_DMAStop(&huart2); //
+		
+    // temp = hdma_usart1_rx.Instance->NDTR;// 获取DMA中未传输的数据个数，NDTR寄存器分析见下面
+		temp = __HAL_DMA_GET_COUNTER(&hdma_usart1_rx);// 获取DMA中未传输的数据个数
+		UART2_Rec_Len = UART2_REC_LEN_MAX - temp;
+		UART2_Rec_Flag = true;
+	}
+#endif
 }
 
 
@@ -137,12 +185,15 @@ void UART2_Data_Analysis(void)
 		printf("接收的数据为：{%s}\r\n",UART2_Rec_Data);
 		memset(UART2_Rec_Data,'\0',sizeof(UART2_Rec_Data));
 		UART2_Rec_Len = 0;
+    #if UART2_ENABLE_DMARX
+    HAL_UART_Receive_DMA(&huart2,UART2_Rec_Data,UART2_REC_LEN_MAX);
+    #endif
 	}
 }
 #endif
 
 
-#if UART3_ENABLE_FLAG
+#if UART3_ENABLE_IRQRX
 /**
  * @Brief:串口中断处理函数
  * @Param:
@@ -152,7 +203,7 @@ void UART2_Data_Analysis(void)
  */
 void MY_UART3_IRQHandler(void)
 {
-
+#if (UART3_ENABLE_DMARX == false)
 	if(__HAL_UART_GET_FLAG(&huart3, UART_FLAG_RXNE) != RESET)
 	{
 		__HAL_UART_CLEAR_FLAG(&huart3,UART_FLAG_RXNE);
@@ -170,6 +221,24 @@ void MY_UART3_IRQHandler(void)
 //		__HAL_UART_CLEAR_FLAG(&huart3,UART_FLAG_IDLE);
 		UART3_Rec_Flag = true;
 	}
+#else
+  uint32_t temp;
+  if(__HAL_UART_GET_FLAG(&huart3, UART_FLAG_IDLE) != RESET)
+	{
+		/*注意：这里只能用__HAL_UART_CLEAR_IDLEFLAG来清除空闲中断标志位*/
+		__HAL_UART_CLEAR_IDLEFLAG(&huart3);
+		
+		temp = huart3.Instance->SR;  //清除状态寄存器SR,读取SR寄存器可以实现清除SR寄存器的功能
+		temp = huart3.Instance->DR; //读取数据寄存器中的数据
+		
+		HAL_UART_DMAStop(&huart3); //
+		
+    // temp = hdma_usart1_rx.Instance->NDTR;// 获取DMA中未传输的数据个数，NDTR寄存器分析见下面
+		temp = __HAL_DMA_GET_COUNTER(&hdma_usart1_rx);// 获取DMA中未传输的数据个数
+		UART3_Rec_Len = UART3_REC_LEN_MAX - temp;
+		UART3_Rec_Flag = true;
+	}
+#endif
 }
 
 
@@ -189,6 +258,9 @@ void UART3_Data_Analysis(void)
 		printf("接收的数据为：{%s}\r\n",UART3_Rec_Data);
 		memset(UART3_Rec_Data,'\0',sizeof(UART3_Rec_Data));
 		UART3_Rec_Len = 0;
+    #if UART3_ENABLE_DMARX
+    HAL_UART_Receive_DMA(&huart3,UART3_Rec_Data,UART3_REC_LEN_MAX);
+    #endif
 	}
 }
 #endif
--- uart_analysis.h
+++ uart_analysis.h
@@ -3,12 +3,12 @@
 
 **--------------File Info---------------------------------------------------------------------------------
 
-** File name:
+** File name:uart_analysis.h
 ** Last modified Date:
 
 ** Last Version:
 
-** Descriptions:
+** Descriptions:这是一个串口UART的IRQRX/DMARX接收库，通过使能对应的宏，可以使用已经分配好的函数和变量，以及设置串口缓冲区大小
 **--------------------------------------------------------------------------------------------------------
 
 ** Created by:		刘家辉
@@ -17,32 +17,44 @@
 ** Version:				V1.0
 ** Descriptions:
 **--------------------------------------------------------------------------------------------------------*/
-#ifndef _UART_IRQ_ANALYSIS_h_
-#define _UART_IRQ_ANALYSIS_h_
+#ifndef _UART_ANALYSIS_h_
+#define _UART_ANALYSIS_h_
 
 #include "User_Conf.h"
 
 /* 对应的串口使能标志位 */
-#define UART1_ENABLE_FLAG		true
-#define UART2_ENABLE_FLAG		false
-#define UART3_ENABLE_FLAG		false
+#define UART1_ENABLE_IRQRX		true
+#define UART2_ENABLE_IRQRX		false
+#define UART3_ENABLE_IRQRX		false
 
 
 #define UART1_REC_LEN_MAX		64		//接收缓冲区大小宏
 #define UART2_REC_LEN_MAX		64		//接收缓冲区大小宏
 #define UART3_REC_LEN_MAX		64		//接收缓冲区大小宏
 
-#if UART1_ENABLE_FLAG
+#if UART1_ENABLE_IRQRX
+#define UART1_ENABLE_DMARX		false	//UART1 DMA接收使能
+extern uint8_t UART1_Rec_Data[UART1_REC_LEN_MAX];	//接收缓冲区数组
+extern uint8_t UART1_Rec_Len;	//接收长度
+extern uint8_t UART1_Rec_Flag;//接收完成标志位
 void MY_UART1_IRQHandler(void);//串口中断处理函数
 void UART1_Data_Analysis(void);//串口数据分析函数
 #endif
 
-#if UART2_ENABLE_FLAG
+#if UART2_ENABLE_IRQRX
+#define UART2_ENABLE_DMARX		true	//UART2 DMA接收使能
+extern uint8_t UART2_Rec_Data[UART2_REC_LEN_MAX];	//接收缓冲区数组
+extern uint8_t UART2_Rec_Len;	//接收长度
+extern uint8_t UART2_Rec_Flag;//接收完成标志位
 void MY_UART2_IRQHandler(void);//串口中断处理函数
 void UART2_Data_Analysis(void);//串口数据分析函数
 #endif
 
-#if UART3_ENABLE_FLAG
+#if UART3_ENABLE_IRQRX
+#define UART3_ENABLE_DMARX		true	//UART3 DMA接收使能
+extern uint8_t UART3_Rec_Data[UART3_REC_LEN_MAX];	//接收缓冲区数组
+extern uint8_t UART3_Rec_Len;	//接收长度
+extern uint8_t UART3_Rec_Flag;//接收完成标志位
 void MY_UART3_IRQHandler(void);//串口中断处理函数
 void UART3_Data_Analysis(void);//串口数据分析函数
 #endif
